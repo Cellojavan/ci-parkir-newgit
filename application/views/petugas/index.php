57fccffdede1cfdc0b4e4a3b6de2e5d63a677f0d
Cellojavan <cellojavan9@gmail.com>
2023-04-14 12:34:28 +0700

    mengedit database

--- application/views/petugas/index.php
+++ application/views/petugas/index.php
@@ -1,20 +1,7 @@
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
-    </ul>
-  </nav>
+ 
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
@@ -160,7 +147,7 @@
     <div class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-8">
+          <div class="col-lg-10">
             <div class="card card-primary card-outline">
               <div class="card-header">
                 <h5 class="m-0">Data User</h5>
